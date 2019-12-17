670a669724255e3410654307593d4a6e5189da3f
Jeff Reiner <theundeadghost@gmail.com>
2019-12-17 17:07:52 +0100

    :sparkles: Having a big ol' donut

--- App.js
+++ App.js
@@ -1,19 +1,516 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import SegmentedControlIOS from "@react-native-community/segmented-control";
+import BigNumber from "bignumber.js";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { ethers } from "ethers";
+import "ethers/dist/shims.js";
+import * as LocalAuthentication from "expo-local-authentication";
+import * as Random from "expo-random";
+import * as SecureStore from "expo-secure-store";
+import React, { useEffect, useState } from "react";
+import {
+  ActionSheetIOS,
+  ActivityIndicator,
+  Alert,
+  Button,
+  Clipboard,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
+import store from "react-native-simple-store";
+import useSWR from "swr";
+import chainIds, { NetworkIdentifier } from "./lib/chainIds";
+import fetcher from "./lib/fetcher";
+
+dayjs.extend(relativeTime);
+
+ethers.errors.setLogLevel("error");
+
+const truncateAddress = address =>
+  `${address.slice(0, 10)}...${address.slice(address.length - 4)}`;
+
+const ACTIVE_NETWORK = chainIds[3];
+
+const API_BASE = "https://ethereum-api.xyz";
+
+const toFixed = (value, decimals) =>
+  new BigNumber(`${value}`).toFixed(decimals).toString();
+
+const formatTimestamp = ts => dayjs(Number(ts)).fromNow();
+
+const parseTxs = (txs, address) =>
+  txs.map(tx => {
+    let action = "";
+    let state = "";
+
+    if (tx.input === "0x") {
+      action = "ETH Transfer";
+    } else if (tx.input !== "0x" && tx.operations.length === 0) {
+      action = "Smart Contract Execution";
+    } else if (tx.operations.length > 0) {
+      action = "ERC-20 Transfer";
+    }
+
+    if (tx.error === true) {
+      state = "Error";
+    } else if (tx.to === tx.from) {
+      state = "Self";
+    } else if (tx.from === address.toLowerCase()) {
+      state = "Sent";
+    } else if (tx.to === address.toLowerCase()) {
+      state = "Received";
+    }
+
+    return {
+      action,
+      state,
+      hash: tx.hash,
+      timestamp: formatTimestamp(tx.timestamp),
+      to: tx.to,
+      from: tx.from,
+      symbol: tx.asset.symbol,
+      value: tx.value,
+      decimals: tx.asset.decimals
+    };
+  });
+
+const parseBalance = bal =>
+  bal.map(({ symbol, decimals, balance, name }) => ({
+    symbol,
+    decimals,
+    balance,
+    native: toFixed(balance, 2),
+    name
+  }));
+
+const WalletTxs = ({ address, chainId }) => {
+  const { data, error } = useSWR(
+    () =>
+      `${API_BASE}/account-transactions?address=${address}&chainId=${chainId}`,
+    fetcher
+  );
+
+  return (
+    <View>
+      {!data && <ActivityIndicator size="large" />}
+
+      {(error || (data && data.success === false)) && (
+        <Text>{JSON.stringify(error)}</Text>
+      )}
+
+      {data &&
+        data.result.length > 0 &&
+        parseTxs(data.result, address).map((tx, i) => {
+          let human = "";
+
+          if (tx.state === "Received") {
+            human = `You received ${tx.value} ${tx.symbol} ${tx.timestamp}`;
+          } else if (tx.state === "Sent") {
+            human = `You sent ${tx.value} ${tx.symbol} to ${truncateAddress(
+              tx.to
+            )} ${tx.timestamp}`;
+          } else if (tx.state === "Self") {
+            human = `You sent ${tx.value} ${tx.symbol} to yourself ${tx.timestamp}`;
+          } else if (tx.state === "Error") {
+            human = "Error";
+          }
+
+          return (
+            <Text key={i} style={{ marginBottom: 16 }}>
+              {human}
+            </Text>
+          );
+        })}
+    </View>
+  );
+};
+
+const WalletBalance = ({ address, chainId }) => {
+  const { data, error } = useSWR(
+    () => `${API_BASE}/account-assets?address=${address}&chainId=${chainId}`,
+    fetcher
+  );
+
+  return (
+    <View>
+      {!data && <ActivityIndicator size="large" />}
+
+      {(error || (data && data.success === false)) && (
+        <Text>{JSON.stringify(error)}</Text>
+      )}
+
+      {data &&
+        data.result.length > 0 &&
+        parseBalance(data.result).map(coin => (
+          <Text key={coin.symbol}>
+            {coin.symbol}: {coin.native}
+          </Text>
+        ))}
+    </View>
+  );
+};
+
+const WalletCollectibles = ({ address }) => {
+  const { data, error } = useSWR(
+    () => `${API_BASE}/account-collectibles?address=${address}`,
+    fetcher
+  );
+
+  return (
+    <View>
+      {!data && <ActivityIndicator size="large" />}
+
+      {(error || (data && data.success === false)) && (
+        <Text>{JSON.stringify(error)}</Text>
+      )}
+
+      {data && <Text>{JSON.stringify(data.result, null, 2)}</Text>}
+    </View>
+  );
+};
 
 export default function App() {
+  const [address, setAddress] = useState("");
+
+  const [activeNetwork, setActiveNetwork] = useState(1);
+
+  useEffect(() => {
+    try {
+      getActiveNetwork();
+
+      getWalletAddress();
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  const loadSeedPhrase = async () => {
+    try {
+      const mnemonic = await SecureStore.getItemAsync(
+        "secureDonutWalletMnemonic"
+      );
+
+      console.log("Found secureDonutWalletMnemonic");
+
+      return mnemonic;
+    } catch (e) {
+      console.log("No secureDonutWalletMnemonic Present");
+
+      console.error(e);
+    }
+  };
+
+  const showSeedPhrase = async () => {
+    try {
+      let results = await LocalAuthentication.authenticateAsync();
+
+      if (results.success) {
+        const mnemonic = await loadSeedPhrase();
+
+        Alert.alert("Your Seed Phrase", mnemonic, [
+          { text: "Copy", onPress: () => Clipboard.setString(mnemonic) }
+        ]);
+      } else {
+        console.log("Failed LocalAuthentication.authenticateAsync!");
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const getActiveNetwork = async () => {
+    try {
+      const network = await store.get("donutActiveNetwork");
+
+      if (!!(await network)) setActiveNetwork(await network);
+    } catch (e) {
+      console.log("No Active Network Set!");
+
+      console.error(e);
+    }
+  };
+
+  const saveActiveNetwork = async network => {
+    try {
+      await store.save("donutActiveNetwork", network);
+
+      setActiveNetwork(network);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const getWalletAddress = async () => {
+    try {
+      const address = await store.get("donutWalletAddress");
+
+      setAddress(await address);
+    } catch (e) {
+      console.log("No Wallet Address Present");
+
+      console.error(e);
+    }
+  };
+
+  const restoreWallet = async mnemonic => {
+    if (await loadSeedPhrase()) {
+      Alert.alert(
+        "Cannot Create New Wallet",
+        "There is already an active wallet for this device"
+      );
+
+      return;
+    }
+
+    try {
+      const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+
+      /**
+       * Store new wallet mnemonic in SecureStorage
+       */
+      SecureStore.setItemAsync("secureDonutWalletMnemonic", mnemonic, {
+        keychainAccessible: SecureStore.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY
+      });
+
+      /**
+       * Store new wallet address in AsyncStorage
+       */
+      await store.save("donutWalletAddress", wallet.address);
+
+      await getWalletAddress();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const restoreWalletPrompt = () => {
+    Alert.prompt(
+      "Import",
+      "Seed phrase",
+      async phrase => await restoreWallet(phrase)
+    );
+  };
+
+  const createNewWallet = async () => {
+    if (await loadSeedPhrase()) {
+      Alert.alert(
+        "Cannot Create New Wallet",
+        "There is already an active wallet for this device"
+      );
+
+      return;
+    }
+
+    const randomBytes = await Random.getRandomBytesAsync(16);
+
+    const wallet = ethers.Wallet.createRandom({ extraEntropy: randomBytes });
+
+    /**
+     * Store new wallet mnemonic in SecureStorage
+     */
+    SecureStore.setItemAsync("secureDonutWalletMnemonic", wallet.mnemonic, {
+      keychainAccessible: SecureStore.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY
+    });
+
+    /**
+     * Store new wallet address in AsyncStorage
+     */
+    await store.save("donutWalletAddress", wallet.address);
+  };
+
+  const deleteWallet = async () => {
+    try {
+      await SecureStore.deleteItemAsync("secureDonutWalletMnemonic");
+
+      await store.delete("donutWalletAddress");
+
+      setAddress("");
+
+      console.log("Wallet Deleted");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const deleteWalletPrompt = () =>
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ["Cancel", "Delete Wallet"],
+        destructiveButtonIndex: 1,
+        cancelButtonIndex: 0,
+        title: "Delete Your Wallet?",
+        message:
+          "You cannot undo this action, deleting your wallet is perminant and cannot be recovered"
+      },
+      buttonIndex => {
+        if (buttonIndex === 1) deleteWallet();
+      }
+    );
+
+  const changeNetwork = () =>
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: [
+          "Cancel",
+          ...Object.keys(chainIds).map(key => chainIds[key].name)
+        ],
+        cancelButtonIndex: 0,
+        title: "Change Network"
+      },
+      buttonIndex => {
+        if (buttonIndex !== 0)
+          saveActiveNetwork(
+            Object.keys(chainIds).map(key => chainIds[key])[buttonIndex - 1].id
+          );
+      }
+    );
+
+  const openSettings = () =>
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: [
+          "Close",
+          "Copy address",
+          "Change network",
+          "Backup seed phrase",
+          "Delete Wallet"
+        ],
+        cancelButtonIndex: 0,
+        title: truncateAddress(address)
+      },
+      buttonIndex => {
+        if (buttonIndex === 1) {
+          Clipboard.setString(address);
+        }
+        if (buttonIndex === 2) changeNetwork();
+
+        if (buttonIndex === 3) showSeedPhrase();
+
+        if (buttonIndex === 4) deleteWalletPrompt();
+      }
+    );
+
+  const [index, setIndex] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <StatusBar barStyle="dark-content" />
+
+      <View style={styles.container}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            ...styles.gutter
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "flex-start",
+              display: "flex"
+            }}
+          >
+            {activeNetwork !== 1 && (
+              <NetworkIdentifier {...chainIds[activeNetwork]} />
+            )}
+          </View>
+
+          <Text
+            accessibilityRole="image"
+            style={{
+              fontSize: 40,
+              height: 48,
+              textAlign: "center"
+            }}
+          >
+            🍩
+          </Text>
+
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "flex-end",
+              display: "flex"
+            }}
+          >
+            <Button title="Settings" onPress={openSettings} />
+          </View>
+        </View>
+
+        {!address && (
+          <View style={styles.section}>
+            <Button onPress={createNewWallet} title="Create New Wallet" />
+            <Button
+              onPress={restoreWalletPrompt}
+              title="Import Existing Wallet"
+            />
+          </View>
+        )}
+
+        {!!address && (
+          <>
+            <View style={styles.gutter}>
+              <View style={{ paddingVertical: 16 }}>
+                <SegmentedControlIOS
+                  values={["Transactions", "Balance", "Profile"]}
+                  selectedIndex={index}
+                  onChange={({ nativeEvent: { selectedSegmentIndex } }) =>
+                    setIndex(selectedSegmentIndex)
+                  }
+                />
+              </View>
+            </View>
+
+            <ScrollView>
+              <View style={styles.gutter}>
+                {index === 0 ? (
+                  <WalletTxs address={address} chainId={activeNetwork} />
+                ) : index === 1 ? (
+                  <WalletBalance address={address} chainId={activeNetwork} />
+                ) : index === 2 ? (
+                  <Text style={{ textAlign: "center", fontSize: 32 }}>👤</Text>
+                ) : null}
+              </View>
+            </ScrollView>
+
+            <View style={styles.gutter}>
+              <View style={{ marginBottom: 24 }}>
+                <Button
+                  onPress={() => Alert.alert("Sending Coming Soon")}
+                  title="Send"
+                />
+              </View>
+            </View>
+          </>
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  gutter: {
+    paddingHorizontal: 20
+  },
+  divider: {
+    height: 1,
+    backgroundColor: "black",
+    width: "100%"
+  },
+  section: { paddingVertical: 24 },
+  container: {
+    flex: 1
+  }
 });
